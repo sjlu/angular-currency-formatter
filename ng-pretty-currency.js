1198f3ebff2d88f903d3723c88fc2c3baf512829
Steven Lu <tacticalazn@gmail.com>
2015-02-11 11:34:15 -0500

    initial commit

---
+++ ng-pretty-currency.js
@@ -0,0 +1,34 @@
+angular.module('ngPrettyCurrency', []).directive('ngPrettyCurrency', function() {
+  return {
+    restrict: 'AE',
+    scope: {
+      ngModel: '=',
+      currencyCode: '='
+    },
+    require: ['^ngModel'],
+    template: '<span class="pretty-currency-symbol" style="margin-right: 4px">{{currencyCode | symbol | html}}</span><span ng-hide="attrs.hideAmount" class="pretty-currency-amount">{{ngModel | number:2}}</span>',
+    link: function($scope, $element, attrs) {
+      $scope.attrs = attrs;
+    }
+  }
+}).filter('html', function($sce) {
+  return function(input) {
+    return $sce.trustAsHtml(input);
+  }
+}).filter('symbol', function() {
+  var codeToSigns = {
+    'PHP': '₱', // philipine peso
+    'ARA': '₳', // Argentine austral
+    'GBP': '£',
+    'USD': '$',
+    'JPY': '¥',
+    'EUR': '€'
+  };
+  return function(input) {
+    input = input.toUpperCase();
+    if (codeToSigns[input]) {
+      return codeToSigns[input];
+    }
+    return input;
+  }
+});
